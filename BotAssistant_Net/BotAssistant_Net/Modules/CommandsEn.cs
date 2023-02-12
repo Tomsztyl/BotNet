e6382170f40b1b1ad86c38f079795343698db26e
Tomsztyl <tomek.jelito@gmail.com>
2023-02-12 12:38:51 +0100

    Initialize project assets

---
+++ BotAssistant_Net/BotAssistant_Net/Modules/CommandsEn.cs
@@ -0,0 +1,60 @@
+﻿using Discord;
+using Discord.Commands;
+
+namespace TutorialBot.Modules
+{
+    public class CommandsEn : ModuleBase<SocketCommandContext>
+    {
+        [Command( "What time is it" )]
+        public async Task TimeNow()
+        {
+            DateTime localDate = DateTime.Now;
+
+            await Context.Channel.SendMessageAsync( "Time: " + localDate + " :flag_pl: " );
+        }
+
+        [Command( "ping" )]
+        public async Task Ping()
+        {
+            await ReplyAsync( "pong" );
+        }
+
+        [Command( "ban" )]
+        [RequireUserPermission( GuildPermission.BanMembers, ErrorMessage = "You don't have the permission ``ban_member``!" )]
+        public async Task BanMember( IGuildUser user = null, [Remainder] string reason = null )
+        {
+            if( user == null )
+            {
+                await ReplyAsync( "Please specify a user!" );
+                return;
+            }
+            if( reason == null ) reason = "Not specified";
+
+            await Context.Guild.AddBanAsync( user, 1, reason );
+
+            var EmbedBuilder = new EmbedBuilder()
+                .WithDescription( $":white_check_mark: {user.Mention} was banned\n**Reason** {reason}" )
+                .WithFooter( footer =>
+                {
+                    footer
+                    .WithText( "User Ban Log" )
+                    .WithIconUrl( "https://i.imgur.com/6Bi17B3.png" );
+                } );
+            Embed embed = EmbedBuilder.Build();
+            await ReplyAsync( embed: embed );
+
+            ITextChannel logChannel = Context.Client.GetChannel( 642698444431032330 ) as ITextChannel;
+            var EmbedBuilderLog = new EmbedBuilder()
+                .WithDescription( $"{user.Mention} was banned\n**Reason** {reason}\n**Moderator** {Context.User.Mention}" )
+                .WithFooter( footer =>
+                {
+                    footer
+                    .WithText( "User Ban Log" )
+                    .WithIconUrl( "https://i.imgur.com/6Bi17B3.png" );
+                } );
+            Embed embedLog = EmbedBuilderLog.Build();
+            await logChannel.SendMessageAsync( embed: embedLog );
+
+        }
+    }
+}
